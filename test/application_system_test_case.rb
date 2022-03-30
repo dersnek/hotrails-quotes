cb8e34ce0e9c93925705274c294858f19fe5a42e
Mark Tityuk <mark.tityuk@gmail.com>
2022-03-30 18:57:49 +0600

    Add the CRUD base of the app

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome do |option|
+    %w[headless no-sandbox disable-setuid-sandbox disable-dev-shm-usage disable-infobars
+      disable-gpu ignore-certificate-errors disable-popup-blocking
+      window-size=1280,1024 disable-features=VizDisplayCompositor].each do |a|
+      option.add_argument(a)
+    end
+  end
 end
